31f51d7d46b8ddd77f44e6bfe8f2b9a2c3a67c6d
Ryan Soanes <Ryan@Ryans-MacBook-Pro.local>
2016-02-05 16:36:38 +0000

    added files for boom

--- boom/ViewController.swift
+++ boom/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func blueButton(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
 
+    @IBAction func redButton(sender: AnyObject) {
+        redBomb.hidden = true
+    }
 }
 
